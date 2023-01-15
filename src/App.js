ebfab5ed590cfeaefd65ea2c24c2165e88b4e51e
Suraj <surajgupta3553@gmail.com>
2023-01-15 20:03:58 +0530

    changes DetailsCard

--- src/App.js
+++ src/App.js
@@ -1,15 +1,11 @@
 import './App.css'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom'
 import Search from './components/Search'
-import DetailsCard from './components/DetailsCard'
 
 const App = () => {
   return (
     <BrowserRouter>
       <Search />
-      <Routes>
-        <Route path='/:id' element={<DetailsCard />} />
-      </Routes>
     </BrowserRouter>
   )
 }
